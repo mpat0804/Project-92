21a8b00fd97926f5054e73ef7da37a41f959aee5
mpat0804 <86076839+mpat0804@users.noreply.github.com>
2022-04-24 10:56:07 -0500

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -4,52 +4,35 @@ player2_name = localStorage.getItem("player2_name");
 player1_score = 0;
 player2_score = 0;
 
-document.getElementById("player1_name").innerHTML=player1_name + ":" ;
-document.getElementById("player2_name").innerHTML=player2_name + ":" ;
+document.getElementById("player1_name").innerHTML = player1_name + " :";
+document.getElementById("player2_name").innerHTML = player2_name + " :";
 
-document.getElementById("player1_score").innerHTML=player1_score ;
-document.getElementById("player2_score").innerHTML=player2_score ;
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
 
-document.getElementById("player_question").innerHTML= "Question turn-" + player1_name + ":" ;
-document.getElementById("player_answer").innerHTML="Answer turn-" + player2_name + ":" ;
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
 
-function send(){
-    getWord=document.getElementById("word").value ;
-    word=getWord.toLowerCase();
-    console.log(word);
-
-    charAt1=word.charAt(1);
-    console.log(charAt1);
-
-    length_div2 = Math.floor(word.length/2);
-    charAt2= word.charAt(length_div2);
-    console.log(charAt2);
-
-    length_minus_1=word.length-1;
-    charAt3=word.charAt(length_minus_1);
-    
-    remove_c1=word.replace(charAt1, "_");    
-    remove_c2=remove_c1.replace(charAt2, "_");
-    remove_c3=remove_c2.replace(charAt3, "_");
-    console.log(remove_c3);
-
-    question="<h4 id='word_display'>Q : "+ remove_c3 + "</h4>";
-    input_box="<br>Answer:<input type='text' id='input'>";
-    check_button = "<br><br><button class='btn btn-info' onClick='check()'>Check</button>";
-    row= question+input_box+check_button;
-    document.getElementById("output").innerHTML=row;
-    document.getElementById("word").value="";
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    answer = parseInt(number1) * parseInt(number2);
+    question_number = "<h5>" + number1 + " X " + number2 + "</h5>";
+    input_box = "<br>Answer : <input type='text' id='input'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
 }
 
 question_turn="player1";
 answer_turn="player2";
 
 function check(){
-    answer=document.getElementById("input").value;
-    input=answer.toLowerCase();
-    console.log(input);
+    number=document.getElementById("input").value;
 
-    if (answer==word){
+    if (number==answer){
         if (answer_turn=="player1"){
             player1_score=player1_score+1;
             document.getElementById("player1_score").innerHTML=player1_score;
